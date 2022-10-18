96920b540b843dc3fdeb7d21e8e65d7af913a834
Vitor <viisanches98@gmail.com>
2022-10-18 18:10:47 -0300

    seo: fix base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,12 @@ import react from '@astrojs/react';
 
 import sitemap from "@astrojs/sitemap";
 
+import { BASE_URL } from './src/env';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [// Enable Preact to support Preact JSX components.
   preact(), // Enable React for the Algolia search component.
   react(), sitemap()],
-  site: `http://astro.build`
+  site: BASE_URL
 });
--- src/env.ts
+++ src/env.ts
@@ -1,3 +1,4 @@
 export const GITHUB_PROJECT_URL = process.env.GITHUB_PROJECT_URL || '';
 export const TELEGRAM_GROUP_URL = process.env.TELEGRAM_GROUP_URL || '';
 export const OG_TWITTER_USER = process.env.OG_TWITTER_USER || '';
+export const BASE_URL = process.env.BASE_URL || 'https://viravoto.org';
